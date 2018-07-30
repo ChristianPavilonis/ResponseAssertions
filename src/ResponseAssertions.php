c2d24b630c538c71ba8007b31f1b86f9e9567fe1
Christian Pavilonis <christian@statebuilt.com>
2018-07-30 07:30:39 -0500

    Adds option to debug on internal server error.

--- src/ResponseAssertions.php
+++ src/ResponseAssertions.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace ChristianPav\ResponseAssertions;
 
 /**
@@ -12,13 +13,34 @@ namespace ChristianPav\ResponseAssertions;
 trait ResponseAssertions
 {
 
+    private $debugOnInternalError = false;
+
     /**
      * @param $response
      * @return TestResponse
      */
     public function createTestResponse($response)
     {
-        return TestResponse::fromBaseResponse($response);
+        $response = TestResponse::fromBaseResponse($response);
+
+        if($response->getStatusCode() == 500 && $this->debugOnInternalError) {
+            $this->kill($response->exception);
+        }
+
+        return $response;
+    }
+
+    public function debugOnInternalError($on = true)
+    {
+        $this->debugOnInternalError = $on;
+    }
+
+    protected function kill($exception)
+    {
+        dd(
+            $exception->getMessage(),
+            $exception->getFile() . ' on line: ' . $exception->getLine()
+        );
     }
 
 }
